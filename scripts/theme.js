2d9b34f11dffe8970f325a266b4cdb54abe4c111
ShibamRoy9826 <royshibam9826@gmail.com>
2024-12-23 16:31:08 +0530

    Added themes

--- scripts/theme.js
+++ scripts/theme.js
@@ -1,20 +1,50 @@
 let rot=0;
 let themeBtn=document.getElementById("themeBtn")
+let themeDiv=document.getElementById("themeDiv")
+let themeSelector=document.getElementById("themes")
+
 let rotated=false;
+
+function changeTheme(themeName){
+  document.documentElement.className=themeName;
+  localStorage.setItem('theme', themeName);
+}
+function dropDown(toopen=true){
+  if (toopen){
+    themeDiv.style.top="12rem";
+    themeDiv.style.left=10;
+  }else{
+    themeDiv.style.top="-50%";
+    themeDiv.style.left=10;
+  }
+}
 
 function themeChanger(){
   if(rotated){
     rot-=90;
     themeBtn.style.transform=`rotate(${rot}deg)`;
     rotated=false;
+    dropDown(false);
+    
   }else{
     rot+=90;
     themeBtn.style.transform=`rotate(${rot}deg)`;
     rotated=true;
+    dropDown();
   }
 }
 
 themeBtn.addEventListener('click',()=>{
-  themeChanger()
+  themeChanger();
 });
 
+themeSelector.addEventListener('change',()=>{
+  changeTheme(themeSelector.value);
+})
+
+window.addEventListener("DOMContentLoaded",()=>{
+  const saved=localStorage.getItem('theme')||'catppuccin';
+  changeTheme(saved);
+  themeSelector.value=saved;
+
+})
