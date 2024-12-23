19e631d9c22d1ad399965df4b91ec72f002d415a
ShibamRoy9826 <royshibam9826@gmail.com>
2024-12-23 21:59:11 +0530

    Added christmas message, added elves image, and made minor changes

--- scripts/countdown.js
+++ scripts/countdown.js
@@ -1,8 +1,8 @@
 hr=document.getElementById("hours")
 mins=document.getElementById("mins")
 secs=document.getElementById("secs")
-let fadeAnim;
 
+let fadeAnim;
 function fade(element) {
       fadeAnim=element.animate(
         [
@@ -19,43 +19,59 @@ function fade(element) {
 
 const christmas= new Date(new Date().getFullYear(),11,25);
 
-let hrPrev=hr.innerHTML;
-let minPrev=mins.innerHTML;
-let secPrev=secs.innerHTML;
+const today=new Date();
+const date=today.getDate();
+const month=today.getMonth()+1;
 
-const timer= setInterval(()=>{
-  const now=new Date();
-  const timeDiff=(christmas-now)/1000;
-  hr.innerHTML=String(Math.floor(timeDiff/(60*60))).padStart(2,'0');
-  mins.innerHTML=String(Math.floor((timeDiff-parseInt(hr.innerHTML,10)*60*60)/60)).padStart(2,'0');
-  secs.innerHTML=String(Math.floor((timeDiff-(parseInt(hr.innerHTML,10)*60*60)-(parseInt(mins.innerHTML,10)*60)))).padStart(2,'0');
+if (date==25 && month==12){
+  let showed=localStorage.getItem('wished')||false;
+  if (showed){
+    showWish();
+    localStorage.setItem('wished', true);
+    let something=document.getElementById('first');
+    let randT=document.getElementById('randomJoke');
+    something.innerHTML="Merry Christmas! Sing carrols and celebrate! Jingle Jingle!"
+    randT.innerHTML="The wait is over! santa is here!"
+  }
+}else{
+  let hrPrev=hr.innerHTML;
+  let minPrev=mins.innerHTML;
+  let secPrev=secs.innerHTML;
 
-  if(hrPrev!=hr.innerHTML){
-    fade(hr);
-    hrPrev=hr.innerHTML;
-  }else{
-    let anims=hr.getAnimations();
-    anims.forEach(anim=>{
-        anim.cancel()
-    })
-  }
-  if(minPrev!=mins.innerHTML){
-    fade(mins);
-    minPrev=mins.innerHTML;
-  }else{
-    let anims=mins.getAnimations();
-    anims.forEach(anim=>{
-        anim.cancel()
-    })
-  }
-  if(secPrev!=secs.innerHTML){
-    fade(secs);
-    secPrev=secs.innerHTML;
-  }else{
-    let anims=secs.getAnimations();
-    anims.forEach(anim=>{
-        anim.cancel()
-    })
-  }
+  const timer= setInterval(()=>{
+    const now=new Date();
+    const timeDiff=(christmas-now)/1000;
+    hr.innerHTML=String(Math.floor(timeDiff/(60*60))).padStart(2,'0');
+    mins.innerHTML=String(Math.floor((timeDiff-parseInt(hr.innerHTML,10)*60*60)/60)).padStart(2,'0');
+    secs.innerHTML=String(Math.floor((timeDiff-(parseInt(hr.innerHTML,10)*60*60)-(parseInt(mins.innerHTML,10)*60)))).padStart(2,'0');
 
-},1000);
+    if(hrPrev!=hr.innerHTML){
+      fade(hr);
+      hrPrev=hr.innerHTML;
+    }else{
+      let anims=hr.getAnimations();
+      anims.forEach(anim=>{
+          anim.cancel()
+      })
+    }
+    if(minPrev!=mins.innerHTML){
+      fade(mins);
+      minPrev=mins.innerHTML;
+    }else{
+      let anims=mins.getAnimations();
+      anims.forEach(anim=>{
+          anim.cancel()
+      })
+    }
+    if(secPrev!=secs.innerHTML){
+      fade(secs);
+      secPrev=secs.innerHTML;
+    }else{
+      let anims=secs.getAnimations();
+      anims.forEach(anim=>{
+          anim.cancel()
+      })
+    }
+
+  },1000);
+}
